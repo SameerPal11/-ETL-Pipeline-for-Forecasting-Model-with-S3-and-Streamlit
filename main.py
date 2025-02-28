c4bf2d0ff3dd16463ebee11fa664ddf2a4ca8744
Sameer Pal <sameer.pal@bellblazetech.com>
2025-02-28 15:31:05 +0530

    Update main.py
    Updated Method to find 
    General Overview for Trends 
    For "Price"

--- main.py
+++ main.py
@@ -1,23 +1,39 @@
 import boto3
 import io
 import pandas as pd
-import streamlit as st
 
 s3 = boto3.client("s3")
 
 BUCKET_NAME = "databucketforprocessing"
 RAW_FILE = "raw_data/car_price_dataset.csv"
-PROCESSED_FILE = "processed_data/"
+PROCESSED_FILE = "processed_data/car_price_dataset.csv"
+FORECAST_FILE = "processed_data/car_price_forecast.csv"
 
 def process_data(data):
     df = pd.read_csv(io.BytesIO(data))
 
-    df.columns = df.columns.str.lower().str.replace(" ", "_")
-
-    df.dropna(inplace=True)
+    df.columns = df.columns.str.lower().str.replace(" ", "_")  
+    df.dropna(inplace=True) 
 
     output = io.StringIO()
     df.to_csv(output, index=False)
+    return df, output.getvalue().encode("utf-8")
+
+def moving_average_forecast(df, window=3, steps=5000):
+    if "price" not in df.columns:
+        raise ValueError("No 'price' column found for forecasting!")
+
+    df["price"] = df["price"].astype(float) 
+
+    df["moving_avg"] = df["price"].rolling(window=window).mean()
+
+    last_avg = df["moving_avg"].dropna().iloc[-1]
+    forecast = [last_avg] * steps  
+    forecast_df = pd.DataFrame({"step": range(1, steps + 1), "forecasted_price": forecast})
+
+    output = io.StringIO()
+    forecast_df.to_csv(output, index=False)
+    
     return output.getvalue().encode("utf-8")
 
 def lambda_handler(event, context):
@@ -25,13 +41,17 @@ def lambda_handler(event, context):
         response = s3.get_object(Bucket=BUCKET_NAME, Key=RAW_FILE)
         raw_data = response["Body"].read()
 
-        processed_data = process_data(raw_data)
+        df, processed_data = process_data(raw_data)
+
+        forecast_data = moving_average_forecast(df, window=3, steps=10)
 
         s3.put_object(Bucket=BUCKET_NAME, Key=PROCESSED_FILE, Body=processed_data)
 
+        s3.put_object(Bucket=BUCKET_NAME, Key=FORECAST_FILE, Body=forecast_data)
+
         return {
             "statusCode": 200,
-            "body": f"File {RAW_FILE} processed and saved as {PROCESSED_FILE}."
+            "body": f"File {RAW_FILE} processed and forecast saved as {PROCESSED_FILE} & {FORECAST_FILE}."
         }
     
     except Exception as e:
@@ -39,15 +59,3 @@ def lambda_handler(event, context):
             "statusCode": 500,
             "body": f"Error processing file: {str(e)}"
         }
-
-
-
-
-
-
-
-
-
-
-
-
